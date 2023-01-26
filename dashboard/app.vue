055096c0df8db1d642bed99c1f098bfd8fb1a3e8
Gabriel Lyra <glyra@lendingclub.com>
2023-01-26 20:32:56 -0300

    Authentication
    Working with credentials: TODO DB lookup
    Github (must include on .env the git client and secret)

--- dashboard/app.vue
+++ dashboard/app.vue
@@ -1,13 +1,5 @@
 <template>
-  <div>
-    <h1 class="bg-slate-500">Dashboard</h1>
-    <p>Here is the dashboard</p>
-    <p>Count: {{ data }}</p>
-    <router-view></router-view>
-  </div>
+  <router-view></router-view>
 </template>
 
-<script setup lang="ts">
-const { data } = await useAsyncData("count", () => $fetch("/api/example"));
-debugger;
-</script>
+<script setup lang="ts"></script>
