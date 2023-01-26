80d92fcc91c82ecd95ba35e4875155c344c6e0b5
Gabriel Lyra <glyra@lendingclub.com>
2023-01-26 09:17:56 -0300

    Setup for auth

---
+++ dashboard/plugins/setup.ts
@@ -0,0 +1,3 @@
+export default defineNuxtPlugin((nitroApp) => {
+  // console.log("Nitro plugin", nitroApp);
+});
--- dashboard/server/interface/types.ts
+++ dashboard/server/interface/types.ts
@@ -1,71 +1,83 @@
 interface Collection {
-    id: string;
-    name: string;
-    timestamps: {
-      createdAt: Date;
-      updatedAt: Date;
-    };
-    userId: string;
-    isDocumentBased: boolean;
-    fields?: { [key: string]: string };
-    public: boolean;
-  }
-  
-  interface Team {
-    id: string;
-    name: string;
-    timestamps: {
-      createdAt: Date;
-      updatedAt: Date;
-    };
-    userId: string;
-    teamMembers: string[];
-  }
-  
-  interface Permission {
-    id: string;
-    name: string;
-    timestamps: {
-      createdAt: Date;
-      updatedAt: Date;
-    };
-    userId: string;
-    collectionId: string;
-  }
-  
-  interface User {
-    id: string;
-    email: string;
-    password: string;
-    name: string;
-    timestamps: {
-      createdAt: Date;
-      updatedAt: Date;
-    };
-    active: boolean;
-  }
-  
-  interface Field {
-    id: string;
-    name: string;
-    type: FieldType;
-    options: {
-      unique?: boolean;
-      maxLength?: number;
-      default?: any;
-      array?: boolean;
-      custom?: any;
-    };
-    userId: string;
-    collectionId: string;
-  }
-  
-  enum FieldType {
-    INT = 'INT',
-    FLOAT = 'FLOAT',
-    TEXT = 'TEXT',
-    JSON = 'JSON',
-    JSONB = 'JSONB',
-    TIMESTAMP = 'TIMESTAMP',
-    CUSTOM = 'CUSTOM',
-  }
+  id: string;
+  name: string;
+  timestamps: {
+    createdAt: Date;
+    updatedAt: Date;
+  };
+  userId: string;
+  isDocumentBased: boolean;
+  fields?: { [key: string]: string };
+  public: boolean;
+}
+
+interface Team {
+  id: string;
+  name: string;
+  timestamps: {
+    createdAt: Date;
+    updatedAt: Date;
+  };
+  userId: string;
+  teamMembers: string[];
+}
+
+interface Permission {
+  id: string;
+  name: string;
+  timestamps: {
+    createdAt: Date;
+    updatedAt: Date;
+  };
+  userId: string;
+  collectionId: string;
+}
+
+interface User {
+  id: string;
+  email: string;
+  password: string;
+  name: string;
+  timestamps: {
+    createdAt: Date;
+    updatedAt: Date;
+  };
+  active: boolean;
+}
+
+interface Field {
+  id: string;
+  name: string;
+  type: FieldType;
+  options: {
+    unique?: boolean;
+    maxLength?: number;
+    default?: any;
+    array?: boolean;
+    custom?: any;
+  };
+  userId: string;
+  collectionId: string;
+}
+
+enum FieldType {
+  INT = "INT",
+  FLOAT = "FLOAT",
+  TEXT = "TEXT",
+  JSON = "JSON",
+  JSONB = "JSONB",
+  TIMESTAMP = "TIMESTAMP",
+  CUSTOM = "CUSTOM",
+}
+
+type UserWithoutPassword = Omit<User, "password">;
+
+export {
+  Collection,
+  Team,
+  Permission,
+  User,
+  UserWithoutPassword,
+  Field,
+  FieldType,
+};
