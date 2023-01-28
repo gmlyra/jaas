9ba13afb4fa9aa83cd0e995a89b92db07aebc278
Ronaldinho <elronaldinho@soccer.net>
2023-01-28 19:28:18 -0300

    Login and Register simple pages

---
+++ dashboard/pages/register.vue
@@ -0,0 +1,79 @@
+<template>
+  <div
+    class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
+  >
+    <div class="w-full max-w-lg space-y-8 bg-gray-100 p-6 rounded-lg">
+      <div>
+        <h1 class="text-4xl font-bold text-center text-indigo-600">JAAS</h1>
+        <p class="mt-2 text-center text-sm text-gray-600">
+          Javascript as a Service
+        </p>
+        <h2
+          class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900"
+        >
+          Register for an account
+        </h2>
+      </div>
+      <form class="mt-8 space-y-4" action="#" method="POST">
+        <div class="-space-y-px rounded-md shadow-sm">
+          <div>
+            <label for="email-address" class="sr-only">Email address</label>
+            <input
+              id="email-address"
+              name="email"
+              type="email"
+              autocomplete="email"
+              required
+              class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              placeholder="Email address"
+            />
+          </div>
+          <div>
+            <label for="password" class="sr-only">Password</label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autocomplete="current-password"
+              required
+              class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              placeholder="Password"
+            />
+          </div>
+          <div>
+            <label for="password-confirm" class="sr-only"
+              >Confirm Password</label
+            >
+            <input
+              id="password-confirm"
+              name="password-confirm"
+              type="password"
+              autocomplete="current-password"
+              required
+              class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              placeholder="Confirm Password"
+            />
+          </div>
+        </div>
+        <div>
+          <button
+            type="submit"
+            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+              <LockClosedIcon
+                class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                aria-hidden="true"
+              />
+            </span>
+            Register
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { LockClosedIcon } from "@heroicons/vue/20/solid";
+</script>
