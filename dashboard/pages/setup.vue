8e6af39550889df23855e8691962ab8f2900df6d
Gabriel Lyra <gmlyra@live.com>
2023-02-01 18:21:48 -0300

    Init setup

---
+++ dashboard/pages/setup.vue
@@ -0,0 +1,127 @@
+<template>
+  <div
+    class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
+  >
+    <div class="w-full max-w-lg space-y-8 bg-gray-100 p-6 rounded-lg">
+      <div>
+        <h1 class="text-4xl font-bold text-center text-indigo-600">JAAS</h1>
+        <p class="mt-2 text-center text-sm text-gray-600">
+          Javascript as a Service
+        </p>
+        <h2
+          class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900"
+        >
+          Sign in to your account
+        </h2>
+      </div>
+      <form class="mt-8 space-y-4" action="#" method="POST">
+        <input type="hidden" name="remember" value="true" />
+        <div class="-space-y-px rounded-md shadow-sm">
+          <div>
+            <label for="email-address" class="sr-only">Email address</label>
+            <input
+              id="email-address"
+              name="email"
+              type="email"
+              autocomplete="email"
+              required
+              class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              placeholder="Email address"
+            />
+          </div>
+          <div>
+            <label for="password" class="sr-only">Password</label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autocomplete="current-password"
+              required
+              class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              placeholder="Password"
+            />
+          </div>
+        </div>
+
+        <div class="flex items-center justify-between">
+          <div class="flex items-center">
+            <input
+              id="remember-me"
+              name="remember-me"
+              type="checkbox"
+              class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+            />
+            <label for="remember-me" class="ml-2 block text-sm text-gray-900"
+              >Remember me</label
+            >
+          </div>
+
+          <div class="text-sm">
+            <a
+              href="#"
+              class="font-medium text-indigo-600 hover:text-indigo-500"
+              >Forgot your password?</a
+            >
+          </div>
+        </div>
+
+        <div class="flex justify-center space-x-4">
+          <button
+            type="submit"
+            class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+              <LockClosedIcon
+                class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                aria-hidden="true"
+              />
+            </span>
+            Sign in
+          </button>
+        </div>
+        <div class="text-center">
+          <span class="text-center text-gray-500">Doen't have an account?</span>
+        </div>
+        <div>
+          <a
+            href="#"
+            class="group relative flex w-full justify-center rounded-md border border-transparent bg-gray-800 py-2 px-4 text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+              <AcademicCapIcon
+                class="h-5 w-5 text-gray-600 group-hover:text-white"
+                aria-hidden="true"
+              />
+            </span>
+            Login with Github
+          </a>
+        </div>
+        <div>
+          <button
+            type="submit"
+            class="group relative flex w-full justify-center rounded-md border border-transparent bg-green-600 py-2 px-4 text-sm font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
+          >
+            <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+              <UserPlusIcon
+                class="h-5 w-5 text-green-500 group-hover:text-green-400"
+                aria-hidden="true"
+              />
+            </span>
+            Sign up
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {
+  LockClosedIcon,
+  AcademicCapIcon,
+  UserPlusIcon,
+} from "@heroicons/vue/20/solid";
+definePageMeta({
+  middleware: "setup",
+});
+</script>
